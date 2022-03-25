cf6763b90d70c5ee0d926b23e7db9a4fc3a3f2ad
avishka05 <IT20642914@my.sliit.lk>
2022-03-25 22:44:49 +0530

    create category initial

---
+++ ITP- source-code/category/app/Http/Controllers/DepartmentController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\department;
+use Illuminate\Http\Request;
+
+class DepartmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $Department= new department();
+        $Department->Description=$request->c_name;
+        $Department->save();
+
+
+
+       return back()->withInput();
+
+
+
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\department  $department
+     * @return \Illuminate\Http\Response
+     */
+    public function show()
+    {
+      $Data= department::all();
+      return view('Department',['department'=>$Data]);
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\department  $department
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(department $department)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\department  $department
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, department $department)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\department  $department
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(department $department)
+    {
+        //
+    }
+}
