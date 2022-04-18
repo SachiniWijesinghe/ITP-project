8fdfe9937ad040ca14cc4755a64e9cc3d4af8a5e
LAKSHAN J.A.T <91153687+LAKSHAN-JAT@users.noreply.github.com>
2022-04-18 23:01:28 +0530

    Create Contact Us page
    User frontend

---
+++ ITP- source-code/custSupFe/Contact_Us/src/parts/header.vue
@@ -0,0 +1,39 @@
+
+<template>
+              <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                  <img src="src/images/logo.png "height="50"  align="left">
+
+
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                      <li class="nav-item">
+                        <a class="nav-link" href="#" position = "absolute" ><font size="5" >|  Home     |</font></a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#" position = "absolute"><font size="5" > New Arrivals       |</font></a>
+                      </li>
+
+                      <li class="nav-item">
+                        <a class="nav-link"href="#" position = "absolute"><font size="5" > Category       |</font></a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="contactus.vue" position = "absolute"><font size="5" > About us        |</font></a>
+                      </li>
+
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#" position = "absolute"><font size="5" >  Contact us </font></a>
+                      </li>
+
+                    </ul>
+
+                    <img src="src/images/mini.png " width="100" height="20" align="right">
+
+
+                  </div>
+
+                </div>
+              </nav>
+
+    </template>
