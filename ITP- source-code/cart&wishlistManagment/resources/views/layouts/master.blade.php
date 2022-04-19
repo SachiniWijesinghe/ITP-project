a93b4beaec558a1a8827d0f3b700feee69bc773f
Chathi19 <chathinirathnayake@gmail.com>
2022-04-19 14:10:28 +0530

    1st

---
+++ ITP- source-code/cart&wishlistManagment/resources/views/layouts/master.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+ <link rel="stylesheet" href="{{ URL::to('src/css/app.css') }}">
+    @yield('styles')
+
+</head>
+<body>
+@include('partials.header')
+
+<div class="container">
+    @yield('content')
+</div>
+
+
+<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+@yield('scripts')
+
+</body>
+</html>
---
+++ ITP- source-code/cart&wishlistManagment/resources/views/shop/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.master')
+@section('title')
+    Cart
+@endsection
+
+
+@section('content')
+
+
+        <br>
+    <div class="row">
+{{--@foreach($productChunk as $product)--}}
+        @foreach($product as $productChunk )
+            <div class="col-sm-6 col-md-3">
+                <div class="img-thumbnail" >
+                    <img
+                        src="{{$productChunk->imagePath}}"  class="img-fluid" alt="responsive image" >
+                    <div class="caption">
+                        <a class="itemName" href=""  target="_blank"><h6>{{$productChunk->title}}</h6></a>
+                        <div class="clearfix">
+                            <div class="pull-left price">LKR {{$productChunk->price}}</div>
+                            <a href="/addToCart/{{$productChunk->id}}" class="btn btn-dark pull-right" role="button">Add To Cart</a></p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        @endforeach
+
+
+    </div>
+{{--    @endforeach--}}
+@endsection
+
+
+
