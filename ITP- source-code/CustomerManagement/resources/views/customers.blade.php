4a004878149230db2d420e4bd1a03b91247e17a5
GeethANURADHA <96642560+GeethANURADHA@users.noreply.github.com>
2022-05-14 08:06:43 +0530

    new table view

--- ITP- source-code/CustomerManagement/resources/views/customers.blade.php
+++ ITP- source-code/CustomerManagement/resources/views/customers.blade.php
@@ -381,43 +381,44 @@
 
 
 
+        <div class="card-body table-responsive p-0" style="height: 400px;">
+                        <table class="table table-head-fixed text-nowrap">
+                            <thead>
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">First Name</th>
+                                <th scope="col">Last Name</th>
+                                <th scope="col"> email</th>
+                                <th scope="col">Password</th>
+                                <th scope="col">Contact No</th>
+                                <th scope="col">Address</th>
+                                <th scope="col">City</th>
+                                <th scope="col">ZIP</th>
+                            </tr>
+                            </thead>
+                            @foreach($candidates as $candidates)
+                                <tbody>
+                                <tr>
+
+                                    <td>{{$candidates['id']}}</td>
+                                    <td>{{$candidates['Fname']}}</td>
+                                    <td>{{$candidates['Lname']}}</td>
+                                    <td>{{$candidates['email']}}</td>
+                                    <td>{{$candidates['password']}}</td>
+                                    <td>{{$candidates['contactNo']}}</td>
+                                    <td>{{$candidates['Address']}}</td>
+                                    <td>{{$candidates['city']}}</td>
+                                    <td>{{$candidates['ZIP']}}</td>
+                                    <td><a href="click-delete/{{$candidates->id}}  " class="btn btn-danger">Delete</a></td>
+                                </tr>
+
+                                </tbody>
+                            @endforeach
+                        </table>
+                    </div>
 
 
-
-        <table class="table table-striped">
-            <thead>
-            <tr>
-
-                <th scope="col">ID</th>
-                <th scope="col">First Name</th>
-                <th scope="col">Last Name</th>
-                <th scope="col"> email</th>
-                <th scope="col">Password</th>
-                <th scope="col">Contact No</th>
-                <th scope="col">Address</th>
-                <th scope="col">City</th>
-                <th scope="col">ZIP</th>
-            </tr>
-            </thead>
-            @foreach($candidates as $candidates)
-                <tbody>
-                <tr>
-
-                    <td>{{$candidates['id']}}</td>
-                    <td>{{$candidates['Fname']}}</td>
-                    <td>{{$candidates['Lname']}}</td>
-                    <td>{{$candidates['email']}}</td>
-                    <td>{{$candidates['password']}}</td>
-                    <td>{{$candidates['contactNo']}}</td>
-                    <td>{{$candidates['Address']}}</td>
-                    <td>{{$candidates['city']}}</td>
-                    <td>{{$candidates['ZIP']}}</td>
-                    <td><a href="click-delete/{{$candidates->id}}  " class="btn btn-danger">Delete</a></td>
-                </tr>
-
-                </tbody>
-            @endforeach
-        </table>
+        
 
 
     </div>
